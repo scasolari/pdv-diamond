5a17b8870c1b1b4934a09966d42db813ea254d70
Sergio Casolari <sergio.casolari@placedv.com>
2026-07-13 16:39:40 +0200

    version 0.0.20 - added search bar

--- components/navigationBar/index.js
+++ components/navigationBar/index.js
@@ -1,15 +1,27 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { Badge } from "@/components/ui/badge"
 import { useSession } from "next-auth/react";
 import { setProfile } from "@/redux/actions/main";
 import { connect } from "react-redux";
 import { useEffect, useState } from "react";
 import useLogout from "@/lib/logout";
 import Avatar, { genConfig } from "react-nice-avatar";
+import {
+    Command,
+    CommandDialog,
+    CommandEmpty,
+    CommandGroup,
+    CommandInput,
+    CommandItem,
+    CommandList,
+    CommandSeparator,
+    CommandShortcut,
+} from "@/components/ui/command"
 
 function NavigationBar(props) {
     const { profile, setProfile } = props;
+    const [open, setOpen] = useState(false)
     const { data: session } = useSession();
     const router = useRouter();
     const logout = useLogout();
@@ -97,6 +109,70 @@ function NavigationBar(props) {
             {linkActive("/app/dashboard")
                 ? <div className="flex flex-col gap-2 justify-between h-[calc(100vh-45px)]">
                     <ul className="p-3 grid gap-1">
+                        <CommandDialog open={open} onOpenChange={setOpen}>
+                            <Command className="dark:bg-neutral-900 bg-neutral-100">
+                                <CommandInput className="!font-semibold !text-xs !h-10 p-0 flex items-center" placeholder="Type a command or search..." />
+                                <CommandList className="pb-1 border-t border-b dark:border-neutral-800 rounded-t-xl dark:bg-neutral-950/40 bg-white">
+                                    <CommandEmpty className="!font-semibold !text-xs p-3">No results found.</CommandEmpty>
+                                    <CommandGroup heading="Action">
+                                        <CommandItem className="!font-semibold !text-xs h-7 rounded-lg flex flex-row gap-2">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.25" stroke-linecap="round" stroke-linejoin="round" class="!h-[14px] lucide lucide-layers-plus-icon lucide-layers-plus"><path d="M12.83 2.18a2 2 0 0 0-1.66 0L2.6 6.08a1 1 0 0 0 0 1.83l8.58 3.91a2 2 0 0 0 .83.18 2 2 0 0 0 .83-.18l8.58-3.9a1 1 0 0 0 0-1.831z"/><path d="M16 17h6"/><path d="M19 14v6"/><path d="M2 12a1 1 0 0 0 .58.91l8.6 3.91a2 2 0 0 0 .825.178"/><path d="M2 17a1 1 0 0 0 .58.91l8.6 3.91a2 2 0 0 0 1.65 0l2.116-.962"/></svg>
+                                            New sanity-check
+                                        </CommandItem>
+                                        <CommandItem className="!font-semibold !text-xs h-7 rounded-lg flex flex-row gap-2">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.25" stroke-linecap="round" stroke-linejoin="round" class="!h-[14px] lucide lucide-settings-icon lucide-settings"><path d="M9.671 4.136a2.34 2.34 0 0 1 4.659 0 2.34 2.34 0 0 0 3.319 1.915 2.34 2.34 0 0 1 2.33 4.033 2.34 2.34 0 0 0 0 3.831 2.34 2.34 0 0 1-2.33 4.033 2.34 2.34 0 0 0-3.319 1.915 2.34 2.34 0 0 1-4.659 0 2.34 2.34 0 0 0-3.32-1.915 2.34 2.34 0 0 1-2.33-4.033 2.34 2.34 0 0 0 0-3.831A2.34 2.34 0 0 1 6.35 6.051a2.34 2.34 0 0 0 3.319-1.915"/><circle cx="12" cy="12" r="3"/></svg>
+                                            Open settings
+                                        </CommandItem>
+                                    </CommandGroup>
+                                    <CommandGroup heading="Recent sanity">
+                                        <CommandItem className="!font-semibold !text-xs h-7 rounded-lg flex flex-row gap-2">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="!h-[14px] lucide lucide-file-icon lucide-file"><path d="M6 22a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h8a2.4 2.4 0 0 1 1.704.706l3.588 3.588A2.4 2.4 0 0 1 20 8v12a2 2 0 0 1-2 2z"/><path d="M14 2v5a1 1 0 0 0 1 1h5"/></svg>
+                                            Mediolanum sanity
+                                        </CommandItem>
+                                    </CommandGroup>
+                                </CommandList>
+                                <div className="p-3">
+                                    <div className="text-xs font-semibold flex flex-row items-center gap-1">
+                                        <div className="flex flex-row items-center gap-1">
+                                            <div className="flex items-center bg-neutral-200 dark:bg-neutral-800 p-0.5 rounded-md">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="!h-[14px] !w-[14px] lucide lucide-arrow-up-icon lucide-arrow-up"><path d="m5 12 7-7 7 7"/><path d="M12 19V5"/></svg>
+                                            </div>
+                                            <div className="flex items-center bg-neutral-200 dark:bg-neutral-800 p-0.5 rounded-md">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="!h-[14px] !w-[14px] lucide lucide-arrow-down-icon lucide-arrow-down"><path d="M12 5v14"/><path d="m19 12-7 7-7-7"/></svg>
+                                            </div>
+                                        </div>
+                                        Navigate
+                                        <div className="flex flex-row items-center gap-1">
+                                            <div className="flex items-center bg-neutral-200 dark:bg-neutral-800 p-0.5 px-1 rounded-md">
+                                                Enter
+                                            </div>
+                                        </div>
+                                        Select
+                                        <div className="flex flex-row items-center gap-1">
+                                            <div className="flex items-center bg-neutral-200 dark:bg-neutral-800 p-0.5 px-1 rounded-md">
+                                                Esc
+                                            </div>
+                                        </div>
+                                        Close
+                                    </div>
+                                </div>
+                            </Command>
+                        </CommandDialog>
+                        <li>
+                            <Link
+                                href="#"
+                                onClick={() => setOpen(!open)}
+                                className={`font-semibold text-xs flex justify-between items-center gap-3 p-1.5 px-2 hover:bg-neutral-100 dark:hover:bg-neutral-800 rounded-lg`}
+                            >
+                                <div className="flex flex-row items-center gap-3">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.25" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-search-icon lucide-search"><path d="m21 21-4.34-4.34"/><circle cx="11" cy="11" r="8"/></svg>
+                                    Search
+                                </div>
+                                <Badge variant="secondary" className="p-0 px-1.5 text-[10px] h-fit font-semibold dark:bg-neutral-700/50 bg-neutral-200/50">
+                                    ⌘K
+                                </Badge>
+                            </Link>
+                        </li>
                         <li>
                             <Link
                                 href="/app/dashboard"
