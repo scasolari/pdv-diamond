24055c91c285aa053e44726011bed35d311744ab
Sergio Casolari <sergio.casolari@placedv.com>
2026-08-31 15:52:56 +0200

    chore: bump version to 0.0.45

--- electron-builder.config.cjs
+++ electron-builder.config.cjs
@@ -24,6 +24,7 @@ module.exports = {
       "zip",
     ],
     icon: "build/icon.icns",
+    notarize: true,
     hardenedRuntime: true,
     gatekeeperAssess: false,
     entitlements: "build/entitlements.mac.plist",
